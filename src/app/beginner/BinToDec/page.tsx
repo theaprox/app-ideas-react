16eeb0fffb3838be3175c937cba86fa11e2e7c54
Aprox <giedrius.punis@gmail.com>
2024-01-13 10:35:41 +0200

    pre-form workflow

--- src/app/beginner/BinToDec/page.tsx
+++ src/app/beginner/BinToDec/page.tsx
@@ -7,27 +7,19 @@ import MemoryIcon from '@mui/icons-material/Memory';
 const BinToDec = () => {
   return (
     <div className='tw-flex-1 tw-grow tw-flex tw-flex-col tw-justify-start tw-items-center tw-pt-24'>
-      <div className='tw-flex-none tw-flex tw-flex-col tw-w-2/3 tw-gap-2 tw-max-w-lg'>
-        <TextField
-          id='standard-basic'
-          label='Binary'
-          variant='outlined'
-          className=''
-        />
-        <TextField
-          id='standard-basic'
-          label='Decimal'
-          variant='outlined'
-          className=''
-        />
-        <Button
-          component='label'
-          color='primary'
-          variant='contained'
-          className=''
-          startIcon={<MemoryIcon />}>
-          Translate
-        </Button>
+      <div className='tw-flex-none tw-flex tw-flex-col tw-w-1/2 tw-gap-2 tw-max-w-md'>
+            <TextField
+              id='standard-basic'
+              label='Binary'
+              variant='outlined'
+              className=''
+            />
+            <TextField
+              id='standard-basic'
+              label='Decimal'
+              variant='outlined'
+            />
+            <Button component="label" color="primary" variant="contained" className='' startIcon={<MemoryIcon />}>Translate</Button>
       </div>
     </div>
   );
