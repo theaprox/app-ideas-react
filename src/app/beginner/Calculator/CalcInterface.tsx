db9a3ef2aeb50c68462e4692101bbb6b59820cf8
Aprox <giedrius.punis@gmail.com>
2024-01-15 14:30:27 +0200

    calc layout and style

--- src/app/beginner/Calculator/CalcInterface.tsx
+++ src/app/beginner/Calculator/CalcInterface.tsx
@@ -1,7 +1,12 @@
 'use client';
-import { Box, Button, TextField } from '@mui/material';
+import { Button, TextField, styled } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import React from 'react';
+
+const CalcButton = styled(Button)({
+  
+
+})
 
 const CalcInterface = () => {
   return (
@@ -17,8 +22,15 @@ const CalcInterface = () => {
             inputProps={{ inputMode: 'numeric', maxLength: 69 }}
             defaultValue={0}
             sx={{
+              m:0,
               '& .MuiInputBase-input': {
                 fontSize: '2em',
+                textAlign: 'right',
+                fontSmooth: 'always',
+              },
+              '& .Mui-disabled': {
+                color: 'white',
+                WebkitTextFillColor: 'rgba(255, 255, 255, 1)',
               }
             }}
           />
@@ -46,17 +58,17 @@ const CalcInterface = () => {
         </Grid>
 
         <Grid item xs={3}>
-          <Button size='large' variant='outlined' fullWidth>
+          <Button size='large' variant='contained' fullWidth>
             7
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button size='large' variant='outlined' fullWidth>
+          <Button size='large' variant='contained' fullWidth>
             8
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button size='large' variant='outlined' fullWidth>
+          <Button size='large' variant='contained' fullWidth>
             9
           </Button>
         </Grid>
@@ -67,17 +79,17 @@ const CalcInterface = () => {
         </Grid>
 
         <Grid item xs={3}>
-          <Button size='large' variant='outlined' fullWidth>
+          <Button size='large' variant='contained' fullWidth>
             4
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button size='large' variant='outlined' fullWidth>
+          <Button size='large' variant='contained' fullWidth>
             5
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button size='large' variant='outlined' fullWidth>
+          <Button size='large' variant='contained' fullWidth>
             6
           </Button>
         </Grid>
@@ -88,17 +100,17 @@ const CalcInterface = () => {
         </Grid>
 
         <Grid item xs={3}>
-          <Button size='large' variant='outlined' fullWidth>
+          <Button size='large' variant='contained' fullWidth>
             1
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button size='large' variant='outlined' fullWidth>
+          <Button size='large' variant='contained' fullWidth>
             2
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button size='large' variant='outlined' fullWidth>
+          <Button size='large' variant='contained' fullWidth>
             3
           </Button>
         </Grid>
@@ -109,12 +121,12 @@ const CalcInterface = () => {
         </Grid>
 
         <Grid item xs={6}>
-          <Button size='large' variant='outlined' fullWidth>
+          <Button size='large' variant='contained' fullWidth>
             0
           </Button>
         </Grid>
         <Grid item xs={3}>
-          <Button size='large' variant='outlined' fullWidth>
+          <Button size='large' variant='contained' fullWidth>
             ,
           </Button>
         </Grid>
